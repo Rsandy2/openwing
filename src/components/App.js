688d7f45c30633ab2feedded60f73e2c5016dddd
Rsandy2 <raesandy2@gmail.com>
2020-11-04 03:13:04 -0500

    Baseline

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,41 @@
 // import React, { Component } from 'react';
-import React, { useState } from 'react';
-import { Navbar, NavItem } from './Navbar';
-import Post from './Post'; 
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
- 
+import firebase, { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Navbar, NavItem } from "./Navbar";
+import Post from "./Post";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAJzEI9t6DSZ-M4OfDgvTOvq523JjIVw4Y",
+  authDomain: "openwing-b6da7.firebaseapp.com",
+  databaseURL: "https://openwing-b6da7.firebaseio.com",
+  projectId: "openwing-b6da7",
+  storageBucket: "openwing-b6da7.appspot.com",
+  messagingSenderId: "1087141256901",
+  appId: "1:1087141256901:web:f5cb5258d5cb448f0e1aac",
+  measurementId: "G-NBNTKS1VHT",
+});
+
+//Global Variables
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
-  const [posts, setPosts] = useState([
-    {
-        postID: "FOX NEWS" ,
-        imageUrl: "https://images3.alphacoders.com/108/thumb-1920-1085992.jpg",
-        postDesc: "People"
-    },
-    {
-        postID: "MSNBC",
-        imageUrl: "https://images2.alphacoders.com/108/thumb-1920-1085635.jpg",
-        postDesc: "Reports"
-    }
-  ])
   return (
     <div className="App">
-      <div className="app__header">
-        {/* Header */}
-        <h1 className="openwing-header-main--logo_container">
-         <a
-            href="https://www.openwing.dev"
-            className="#"
-            title="Openwing">
-              Openwing
-          </a>
-        </h1>
-        
-      <Navbar>
-        <NavItem item_text="Start"/>
-        <NavItem item_text="Start"/>
-        <NavItem item_text="Start"/>
-      </Navbar>
-
-      
-      </div>
-         <div className="post">
-        {/* Post */}
-
-            {
-              posts.map(post => (
-                <Post 
-                  postID={post.postID} 
-                  imageUrl={post.imageUrl}
-                  postDesc={post.postDesc}
-                />))
-            }
-
-     
-        
-        {/* <Post /> */}
-        {/* Post */}
-        </div>
-      </div>
-    )
-  }
-
- 
-
+      <Post
+        postID="Mountains"
+        imageURL="https://images4.alphacoders.com/110/1109932.jpg"
+        postDESC="A nice view."
+      />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,55 +1,50 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
-import './Post.css';
-import { deepOrange, green } from '@material-ui/core/colors';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import "./Post.css";
+import { deepOrange, green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
     },
-    square: {
-      color: theme.palette.getContrastText(deepOrange[500]),
-      backgroundColor: deepOrange[500],
-    },
-    rounded: {
-      color: '#fff',
-      backgroundColor: green[500],
-    },
-  }));
+  },
+  square: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  rounded: {
+    color: "#fff",
+    backgroundColor: green[500],
+  },
+}));
 
-function Post({ postID, postDesc, imageUrl}) {
-    const classes = useStyles();
-    return (
-        <div className="post__container">
-            {/* Header -> News Icon + Network Name */}
-            <div className="post__header">
-            <Avatar 
-            variant="square" 
-            className="post__Avatar"
-            alt='FOXNEWS'
-            src="/static/images/avatar/1.jpg"
-            />
-                
-             
-            <h2>{postID}</h2>
+function Post({ postID, postDESC, imageURL }) {
+  const classes = useStyles();
+  return (
+    <div className="post__container">
+      {/* Header -> News Icon + Network Name */}
+      <div className="post__header">
+        <Avatar
+          variant="square"
+          className="post__Avatar"
+          alt="FOXNEWS"
+          src="/static/images/avatar/1.jpg"
+        />
 
-            </div>
-            
-            {/* Image */}
-            <img 
-            className="post__image"
-            src={imageUrl}
-            />
-            {/* Description + Text */}
-            <div className="post__text">
-                <p>{postDesc}</p>
-            </div>
-        </div>
-    )
+        <h2>{postID}</h2>
+      </div>
+
+      {/* Image */}
+      <img className="post__image" src={imageURL} />
+      {/* Description + Text */}
+      <div className="post__text">
+        <p>{postDESC}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
