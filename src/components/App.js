066a9a454e4ea0ea6f3037ad2b7436cf19bbef31
Rsandy2 <raesandy2@gmail.com>
2020-11-04 03:17:02 -0500

    Revised

--- src/components/App.js
+++ src/components/App.js
@@ -11,17 +11,6 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Navbar, NavItem } from "./Navbar";
 import Post from "./Post";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAJzEI9t6DSZ-M4OfDgvTOvq523JjIVw4Y",
-  authDomain: "openwing-b6da7.firebaseapp.com",
-  databaseURL: "https://openwing-b6da7.firebaseio.com",
-  projectId: "openwing-b6da7",
-  storageBucket: "openwing-b6da7.appspot.com",
-  messagingSenderId: "1087141256901",
-  appId: "1:1087141256901:web:f5cb5258d5cb448f0e1aac",
-  measurementId: "G-NBNTKS1VHT",
-});
-
 //Global Variables
 const auth = firebase.auth();
 const firestore = firebase.firestore();
